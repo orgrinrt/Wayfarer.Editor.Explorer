421fcf6f4a8f9ebd88899d9238f1b30fd68990a6
orgrinrt <ollipekka.toimela@gmail.com>
2019-08-02 19:12:40 +0300

    Finished the base behaviour for the EditorExplorer, works for now until we start adding extra features

--- EditorExplorerDock.cs
+++ EditorExplorerDock.cs
@@ -11,32 +11,42 @@ namespace Wayfarer.Editor.Explorer
     [Tool]
     public class EditorExplorerDock : Control
     {
-        //[Get("VBox")] private VBoxContainer _main;
+        [Get("VBox")] private VBoxContainer _main;
 
-        //private Tree _tree;
+        private Tree _tree;
+        private ExplorerPlugin _plugin;
     
         public override void _Ready()
         {
-            //this.SetupWayfarer();
-            /*
+            this.SetupWayfarer();
+            
             try
             {
                 _tree = _plugin.GetEditorTree();
-                _tree = new Tree();
                 _tree.Name = "EditorHierarchy";
+                _tree.AnchorBottom = 1;
+                _tree.AnchorRight = 1;
+                _tree.SizeFlagsHorizontal = (int)SizeFlags.ExpandFill;
+                _tree.SizeFlagsVertical = (int)SizeFlags.ExpandFill;
                 _main.AddChild(_tree);
+                Log.Wf.Editor("EditorTree added!", true);
                 
             }
             catch (Exception e)
             {
                 Log.Wf.EditorError("Couldn't add the ExplorerPlugin.GetEditorTree() tree to the dock", e, true);
-            }*/
+            }
         }
-/*
+
         public override void _ExitTree()
         {
             Log.Wf.Editor("EditorExplorerDock is being queued free", true);
-        }*/
+        }
+
+        public void SetPlugin(ExplorerPlugin plugin)
+        {
+            _plugin = plugin;
+        }
     }
 }
 
--- ExplorerPlugin.cs
+++ ExplorerPlugin.cs
@@ -2,8 +2,12 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using Godot.Collections;
+using Wayfarer.Core.Systems;
+using Wayfarer.Core.Utils.Coroutine;
 using Wayfarer.Core.Utils.Debug;
 using Wayfarer.Core.Utils.Helpers;
 
@@ -19,6 +23,7 @@ namespace Wayfarer.Editor.Explorer
         
         public override void _EnterTree()
         {
+            EnablePlugin();
             
             
         }
@@ -27,11 +32,41 @@ namespace Wayfarer.Editor.Explorer
         {
             try
             {
-                //CallDeferred(nameof(AddCustomContainers));
+                Log.Wf.Editor("ExplorerPlugin Ready, starting editor adding process!", true);
+                CallDeferred(nameof(StartProcessToAddContainers));
             }
             catch (Exception e)
             {
                 Log.Wf.EditorError("Couldn't add custom controls in ExplorerPlugin (_EnterTree)", e, true);
+            }
+
+            DisablePlugin();
+        }
+
+        public override void ApplyChanges()
+        {
+            try
+            {
+                Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
+
+                foreach (Node node in editorNodes)
+                {
+                    if (node is Iterator)
+                    {
+                        try
+                        {
+                            Log.Wf.Editor("Found Iterator: " + node.GetParent().Name + "->" + node.Name + ", " + node.GetType(), true);
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Wf.EditorError("Tried to QueueFree() EditorExplorerDock, but couldn't", e, true);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't Remove old EditorExplorer", e, true);
             }
         }
 
@@ -51,26 +86,123 @@ namespace Wayfarer.Editor.Explorer
                 {
                     RemoveOldExplorer();
                 }
-                catch (Exception exception)
+                catch (Exception e2)
                 {
-                    Log.Wf.EditorError("... that didn't work either, failing hard and crashing", e, true);
+                    Log.Wf.EditorError("... that didn't work either, failing hard and crashing", e2, true);
                 }
+            }
+        }
+
+        private void StartProcessToAddContainers()
+        {
+            try
+            {
+                RemoveOldExplorer(GetSameControlAsScene());
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't remove the OLD EditorExplorerDock via TabContainer parent, trying the recursive find...", e, true);
+
+                try
+                {
+                    RemoveOldExplorer();
+                }
+                catch (Exception e2)
+                {
+                    Log.Wf.EditorError("... that didn't work either, failing hard and crashing", e2, true);
+                }
+            }
+            
+            try
+            {
+                Node baseControl = EditorInterface.GetBaseControl();
+                Godot.Collections.Array children = baseControl.GetChildren();
+                Godot.Collections.Array iterators = new Godot.Collections.Array();
+                Iterator iterator;
+
+                foreach (Node child in children)
+                {
+                    if (child is Iterator i)
+                    {
+                        iterators.Add(i);
+                    }
+                }
+                
+                if (iterators.Count > 1)
+                {
+                    Log.Wf.EditorError("There were MULTIPLE ITERATORS, this shouldn't happen (" + iterators.Count + ")", true);
+                    iterator = (Iterator) iterators.Last();
+
+                    for (int i = 0; i < iterators.Count - 1; i++)
+                    {
+                        Iterator it = (Iterator) iterators[i];
+                        try
+                        {
+                            it.QueueFree();
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Wf.EditorError("Couldn't QueueFree the extra iterators...?", e, true);
+                        }
+                    }
+                }
+                else
+                {
+                    iterator = (Iterator) iterators[0];
+                }
+
+                if (iterator == null)
+                {
+                    iterator = baseControl.GetNodeOfType<Iterator>();
+                }
+
+                iterator.Name = "EditorIterator";
+                
+                try
+                {
+                    if (iterator?.EditorCoroutine != null)
+                    {
+                        iterator.EditorCoroutine.Run(WaitAndThenAddContainers());
+                    }
+                    else
+                    {
+                        Log.Wf.EditorError("The EditorIterator was null!?", true);
+                    }
+                    
+                    Log.Wf.Editor("COROUTINE AMOUNT = " + iterator?.EditorCoroutine?.Count);
+                }
+                catch (Exception e)
+                {
+                    Log.Wf.EditorError("Couldn't start an EditorCoroutine... is the EditorCoroutine instantiated yet?", e, true);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't find reference to the Iterator... could this be because it's not instantiated yet?", e, true);
             }
         }
 
         private IEnumerator WaitAndThenAddContainers()
         {
+            Log.Wf.Editor("Starting WaitAndThenAddContainers() in ExplorerPlugin");
             bool first = true;
             while (true)
             {
                 if (!first)
                 {
+                    Log.Wf.Editor("Adding the EditorExplorer containers!");
                     AddCustomContainers();
+                    break;
                 }
 
                 first = false;
-                yield return 4f;
+                yield return 1f;
             }
+        }
+
+        private void EnsureThatEditorDockExists()
+        {
+            
         }
 
         private void AddCustomContainers()
@@ -79,6 +211,7 @@ namespace Wayfarer.Editor.Explorer
             {
                 PackedScene dockScene = GD.Load<PackedScene>("res://Addons/Wayfarer.Editor.Explorer/Assets/Scenes/EditorExplorerDock.tscn");
                 _dock = (EditorExplorerDock) dockScene.Instance();
+                _dock.SetPlugin(this);
                 Control tab = GetSameControlAsScene();
                 if (tab != null)
                 {
@@ -148,6 +281,7 @@ namespace Wayfarer.Editor.Explorer
                     {
                         TreeItem item = tree.CreateItem(rootItem);
                         item.SetText(nameColumn, child.Name + "(" + child.GetType() + ")");
+                        item.Collapsed = true;
                 
                         PopulateTreeItemsFromNodeChildrenRecursive(tree, child, item, nameColumn);
                     }
@@ -161,62 +295,84 @@ namespace Wayfarer.Editor.Explorer
 
         private Control GetSameControlAsScene()
         {
-            Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
-
-            foreach (Node node in editorNodes)
+            try
             {
-                if (node is TabContainer tabContainer)
+                Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
+
+                foreach (Node node in editorNodes)
                 {
-                    foreach (Control child in tabContainer.GetChildren())
+                    if (node is TabContainer tabContainer)
                     {
-                        if (child.Name == "Scene")
+                        foreach (Control child in tabContainer.GetChildren())
                         {
-                            return tabContainer;
+                            if (child.Name == "Scene")
+                            {
+                                return tabContainer;
+                            }
                         }
                     }
                 }
             }
-
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't GetSameControlAsScene (ExplorerPlugin)", e, true);
+            }
+            
             return null;
         }
         
         private void RemoveOldExplorer()
         {
-            Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
-
-            foreach (Node node in editorNodes)
+            try
             {
-                if (node is EditorExplorerDock)
+                Node[] editorNodes = EditorInterface.GetBaseControl().GetChildrenRecursive();
+
+                foreach (Node node in editorNodes)
                 {
-                    try
+                    if (node is EditorExplorerDock)
                     {
-                        node.QueueFree();
-                        Log.Wf.Editor("Removed old EditorExplorerDock (QueueFree)", true);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.Wf.EditorError("Tried to QueueFree() EditorExplorerDock, but couldn't", e, true);
+                        try
+                        {
+                            node.QueueFree();
+                            Log.Wf.Editor("Removed old EditorExplorerDock (QueueFree)", true);
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Wf.EditorError("Tried to QueueFree() EditorExplorerDock, but couldn't", e, true);
+                        }
                     }
                 }
             }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't Remove old EditorExplorer", e, true);
+            }
+            
         }
 
         private void RemoveOldExplorer(Control parent)
         {
-            foreach (Node node in parent.GetChildren())
+            try
             {
-                if (node is EditorExplorerDock)
+                foreach (Node node in parent.GetChildren())
                 {
-                    try
+                    if (node is EditorExplorerDock)
                     {
-                        node.QueueFree();
-                        Log.Wf.Editor("Removed old EditorExplorerDock (QueueFree)", true);
-                    }
-                    catch (Exception e)
-                    {
-                        Log.Wf.EditorError("Tried to QueueFree() EditorExplorerDock, but couldn't", e, true);
+                        try
+                        {
+                            node.QueueFree();
+                            Log.Wf.Editor("Removed old EditorExplorerDock (QueueFree) (with defined parent)", true);
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Wf.EditorError("Tried to QueueFree() EditorExplorerDock, but couldn't (with defined parent)", e, true);
+                        }
                     }
                 }
+            }
+            catch (Exception e)
+            {
+                Log.Wf.EditorError("Couldn't Remove old EditorExplorer (with defined parent)", e, true);
             }
         }
     }
